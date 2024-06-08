6155bfd70028a1ea68c44966093a3dff59cf3591
mukeshkuiry <mukeshkk3162@gmail.com>
2024-06-09 00:44:13 +0530

    page responsive and edit team

--- public/data/teamsData.ts
+++ public/data/teamsData.ts
@@ -3,156 +3,135 @@
 // import rish from '../assets/team/rish.png';
 // import pani from '../assets/team/pani.png';
 
-import uttaran from '../assets/uttaran.jpg';
-import anuran from '../assets/team/anuran.png';
-import rish from '../assets/team/rishita.png';
-import pani from '../assets/team/saptarshi.png';
-import debayan from '../assets/team/debayan.png';
-import aish from '../assets/team/aishwarya.png';
+import uttaran from "../assets/uttaran.jpg";
+import anuran from "../assets/team/anuran.png";
+import pani from "../assets/team/saptarshi.png";
+import debayan from "../assets/team/debayan.png";
+import mukesh from "../assets/team/mukesh.jpeg";
 
 export const teamsData = [
-    {
+  {
+    id: 1,
+    name: "Uttaran Nayak",
+    position: "Chief Executive Officer",
+    profileimg: uttaran,
+    achievements: [
+      {
         id: 1,
-        name : 'Uttaran Nayak',
-        position : 'Chief Executive Officer',
-        profileimg : uttaran,
-        achievements : [
-            {
-                id: 1,
-                value : 'Economic Times and Unstop Competitive Leader \'23'
-            },
-            {
-                id : 2,
-                value : 'National Entrepreneurship Summit Winner \'21 and \'23'
-            },
-            {
-                id :3,
-                value : `Asia Pacific Global Hackathon Winner '23`
-            },
-            {
-                id: 4,
-                value : 'TEDx Speaker, upcoming Josh Talks speaker'
-            }
-        ]
-    },
-    {
+        value: "Economic Times and Unstop Competitive Leader '23",
+      },
+      {
         id: 2,
-        name : 'Anuran Roy',
-        position : 'Chief Technology Officer',
-        profileimg : anuran,
-        achievements : [
-            {
-                id: 1,
-                value : 'Ex - AI Engineering Team @AI4Bharat'
-            },
-            {
-                id : 2,
-                value : 'Deep Learning & AI Research Intern, @Google DeepMind'
-            },
-            {
-                id :3,
-                value : 'Ex- Founding AI Engineering team, @Quantum Leap AI'
-            },
-            {
-                id: 4,
-                value : 'Full Stack AI Engineer, Memorang'
-            }
-        ]
-    },
-    {
+        value: "National Entrepreneurship Summit Winner '21 and '23",
+      },
+      {
         id: 3,
-        name : 'Rishita Shaw',
-        position : 'Head of Technical Research',
-        profileimg : rish,
-        achievements : [
-            {
-                id: 1,
-                value : 'Open Source Summit Speaker, Japan ‘23'
-            },
-            {
-                id : 2,
-                value : 'Microsoft Imagine Cup winner ‘22'
-            },
-            {
-                id :3,
-                value : 'Research Papers on DevOps, Cloud  & Security'
-            },
-            {
-                id: 4,
-                value : 'Economic Times & Unstop Competitive Leader ‘23'
-            }
-        ]
-    },
-    {
+        value: `Asia Pacific Global Hackathon Winner '23`,
+      },
+      {
         id: 4,
-        name : 'Saptarshi Pani',
-        position : 'Chief Data Scientist',
-        profileimg : pani,
-        achievements : [
-            {
-                id: 1,
-                value : 'Richard E. Merwin Internation Scholar'
-            },
-            {
-                id : 2,
-                value : '3 Research Papers in Machine Learning (International Journals)'
-            },
-            {
-                id :3,
-                value : ' 10x National Hackathon Winner'
-            },
-            {
-                id: 4,
-                value : 'Economic Times & Unstop Competitive Leader ‘23'
-            }
-        ]
-    },
-    {
-        id: 5,
-        name : 'Debayan Pradhan',
-        position : 'Full Stack Web Developer',
-        profileimg : debayan,
-        achievements : [
-            {
-                id: 1,
-                value : 'Incubatee under Algorand Start-up Labs'
-            },
-            {
-                id : 2,
-                value : 'National Runner-Up, Hack4Bengal 2023 '
-            },
-            {
-                id :3,
-                value : 'National Finalist, Smart India Hackathon 2023'
-            },
-            {
-                id: 4,
-                value : 'Google Developers Community Lead, KGEC Kalyani'
-            }
-        ]
-    },
-    {
+        value: "TEDx Speaker, upcoming Josh Talks speaker",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Anuran Roy",
+    position: "Chief Technology Officer",
+    profileimg: anuran,
+    achievements: [
+      {
+        id: 1,
+        value: "Ex - AI Engineering Team @AI4Bharat",
+      },
+      {
         id: 2,
-        name : 'Aishwarya Daga',
-        position : 'ML Intern',
-        profileimg : aish,
-        achievements : [
-            {
-                id: 1,
-                value : ' Built Custom Chatbots, Large Language Models (LLMs)'
-            },
-            {
-                id : 2,
-                value : 'Focussed on building Prediction Models and Data Analysis'
-            },
-            {
-                id :3,
-                value : 'Ex- AI engineering Intern, Pi Data Centers'
-            },
-            {
-                id: 4,
-                value : 'Computer Science & Engineering, VIT Vellore'
-            }
-        ]
-    }
-]
+        value: "Deep Learning & AI Research Intern, @Google DeepMind",
+      },
+      {
+        id: 3,
+        value: "Ex- Founding AI Engineering team, @Quantum Leap AI",
+      },
+      {
+        id: 4,
+        value: "Full Stack AI Engineer, Memorang",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Saptarshi Pani",
+    position: "Chief Data Scientist",
+    profileimg: pani,
+    achievements: [
+      {
+        id: 1,
+        value: "Richard E. Merwin Internation Scholar",
+      },
+      {
+        id: 2,
+        value: "3 Research Papers in Machine Learning (International Journals)",
+      },
+      {
+        id: 3,
+        value: " 0x National Hackathon Winner",
+      },
+      {
+        id: 4,
+        value: "Economic Times & Unstop Competitive Leader ‘23",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Debayan Pradhan",
+    position: "Full Stack Web Developer",
+    profileimg: debayan,
+    achievements: [
+      {
+        id: 1,
+        value: "Incubatee under Algorand Start-up Labs",
+      },
+      {
+        id: 2,
+        value: "National Runner-Up, Hack4Bengal 2023 ",
+      },
+      {
+        id: 3,
+        value: "National Finalist, Smart India Hackathon 2023",
+      },
+      {
+        id: 4,
+        value: "Google Developers Community Lead, KGEC Kalyani",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Mukesh Kuiry",
+    position: "Lead Cloud & Backend dev",
+    profileimg: mukesh,
+    achievements: [
+      {
+        id: 1,
+        value: "Ex - Golang & Kubernetes Engineer @justk8s",
+      },
+      {
+        id: 2,
+        value: "Ex - Golang & Kubernetes Engineer @forge",
+      },
+      {
+        id: 3,
+        value: "Ex - Founding developer @freeflow",
+      },
+      {
+        id: 4,
+        value: "Ex - Software developer @chainrisk(prev UNSNARL)",
+      },
+      {
+        id: 5,
+        value: "Maintainer of multiple open source projects @clueless",
+      },
+    ],
+  },
+];
